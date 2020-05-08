de0f6f95eaeb8b2a8f57c7233d5495c5cc8a46eb
shobaseprg <pgymlsho19128@gmail.com>
2020-05-08 21:00:18 +0900

    ログアウト時にオフ

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -14,9 +14,11 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    current_user.on_off = 0
+    current_user.save
+    super
+  end
 
   # protected
 
