350f47a2d4e5aadc703fee38906ed0f0fdddb673
shobaseprg <pgymlsho19128@gmail.com>
2020-05-08 21:11:58 +0900

    Revert "Revert "リターン""
    This reverts commit 501bd6ede3ba15ff2ef7ec5dd41080d1ad08f0ad.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :timeoutable
 
          has_many :posts
+#  ===================================
+#  カスタムバリデーション
+#  ===================================
+VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+# ===================================
+# アソシエーション
+# ===================================
+has_many :posts
+#  ===================================
+#  バリデーション
+#  ===================================
+validates :name, presence: true, uniqueness: true,length: { maximum: 20 }
+validates :slack, presence: true, length: { maximum: 50 }
+validates :period, presence: true, inclusion: {in: 50..999 }       
+validates :email, presence: true, uniqueness: true
+validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
