220ea70a38f6f96114be8ac94ae09b0d0733e52c
shobaseprg <pgymlsho19128@gmail.com>
2020-05-09 00:03:47 +0900

    日本語名　エラー時ルーティングエラー回避

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   } 
 
   devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy" 
+    get "sign_out", to: redirect("users/sessions#destroy") 
+    get '/users', to: redirect("/users/sign_up")
   end
 
   
