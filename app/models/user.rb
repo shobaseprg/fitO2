fb754d3185ee2a2d48f9118c0ab5391e52efa37e
shobaseprg <pgymlsho19128@gmail.com>
2020-05-15 18:10:29 +0900

    編集時のパスワードもバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,9 +20,9 @@ belongs_to_active_hash :team
 #  ===================================
 validates :name, presence: true, uniqueness: true,length: { maximum: 20 }
 validates :slack, presence: true, length: { maximum: 50 }
-validates :period, presence: true, inclusion: {in: 50..999 }       
+validates :period, presence: true, inclusion: {in: 50..200 }       
 validates :style, presence: true, inclusion: {in: 1..2 } 
 validates :team_id, presence: true
 validates :email, presence: true, uniqueness: true
-validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }, on: :create
+validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
 end
