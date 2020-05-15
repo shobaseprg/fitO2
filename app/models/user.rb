bd8562fa8914682c0aea4b1ddd808baf9d91d6ff
shobaseprg <pgymlsho19128@gmail.com>
2020-05-15 22:53:42 +0900

    Revert "id にアルファべって"
    This reverts commit ca07eae19952988d703bc4453ba5822b95868f56.

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ validates :name, presence: true, uniqueness: true,length: { maximum: 20 }
 validates :slack, presence: true, length: { maximum: 50 }
 validates :period, presence: true, inclusion: {in: 50..200 }       
 validates :style, presence: true, inclusion: {in: 1..2 } 
-validates :team_id, presence: true
+validates :team_id, presence: true,inclusion: {in: 1..26 } 
 validates :email, presence: true, uniqueness: true
 validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }, on: :create
 end
