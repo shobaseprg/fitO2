b65b6cf36759a41c22eca62afb2e248188d21572
shobaseprg <pgymlsho19128@gmail.com>
2020-05-18 23:45:05 +0900

    両方３０００削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "fitO2_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'http://54.238.67.129' , port: 3000 } 
+  config.action_mailer.default_url_options = { host: 'http://54.238.67.129'  } 
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
