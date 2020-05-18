94d29f0f99f772f5bf06f300c02f9c0f47fd1d03
shobaseprg <pgymlsho19128@gmail.com>
2020-05-19 00:04:22 +0900

    パスワードを秘匿化

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   address: 'smtp.gmail.com',
   port: 587,
   user_name: 'testmaildevise@gmail.com',
-  password: 'yzioipkxnpujgqry',
+  password: Rails.application.credentials[:emailpass]
   authentication: 'plain',
   enable_starttls_auto: true
 }
