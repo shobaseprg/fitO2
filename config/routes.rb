988ba0720aef3bb0170af115046ba372316a01b5
shobaseprg <pgymlsho19128@gmail.com>
2020-05-18 21:46:37 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
-    :passwords => 'users/password/edit' 
+    :passwords => 'users/password' 
   } 
 
   devise_scope :user do
